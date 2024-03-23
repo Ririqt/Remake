3b7d493d3fc98e0ba3355aca64a0273924a73938
Rei Rigos <mcrei1018@gmail.com>
2024-03-23 19:51:34 +0800

    Displaying Output With Style

--- Remake_Print.py
+++ Remake_Print.py
@@ -4,8 +4,10 @@ font_1 = Figlet(font="epic")
 your_name = str(input("Enter Your Name: "))
 your_dream_job = str(input("Enter Your Dream Job: "))
 
-print("Hi" + your_name + "!")
-print("I hope you will become a " + your_dream_job + "!")
+text_art_1 = (font_1.renderText("Hi" + your_name + "!"))
+text_art_2 = (font_1.renderText("I hope you will become a " + your_dream_job + "!"))
+
+print('\033[95m' + text_art_1 + text_art_2)
 
 your_birth_month = str(input(your_name + "!" + "When is your birth month?: "))
 your_school = str(input("What school do you will pursue " + your_dream_job + "?: "))
