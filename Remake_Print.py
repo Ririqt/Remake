f27f85c8511378273e370559d430d024dd2f54d9
Rei Rigos <mcrei1018@gmail.com>
2024-03-23 19:43:35 +0800

    Creating Font Style for First Questions

--- Remake_Print.py
+++ Remake_Print.py
@@ -1,11 +1,15 @@
+from pyfiglet import Figlet
+
+font_1 = Figlet(font="epic")
 your_name = str(input("Enter Your Name: "))
 your_dream_job = str(input("Enter Your Dream Job: "))
 
 print("Hi" + your_name + "!")
 print("I hope you will become a " + your_dream_job + "!")
 
-your_birth_month = str(input(your_name + "!" + "When is your birth month? "))
+your_birth_month = str(input(your_name + "!" + "When is your birth month?: "))
 your_school = str(input("What school do you will pursue " + your_dream_job + "?: "))
 
 print("Have a blast on " + your_birth_month + "!")
 print("Study Hard at " + your_school + "!")
+
