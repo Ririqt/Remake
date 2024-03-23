2f766d1d302e9baf7859518d2cf1f2dee3326c43
Rei Rigos <mcrei1018@gmail.com>
2024-03-23 18:39:19 +0800

    Displaying Outputs

--- Remake_Print.py
+++ Remake_Print.py
@@ -1,2 +1,5 @@
 your_name = str(input("Enter Your Name: "))
 your_dream_job = str(input("Enter Your Dream Job: "))
+
+print("Hi" + your_name + "!")
+print("I hope you will become a " + your_dream_job + "!")
