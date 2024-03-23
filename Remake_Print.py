55d50f4ed16aa947abaf809376af1fa50bd9b280
Rei Rigos <mcrei1018@gmail.com>
2024-03-23 18:46:22 +0800

    Inputting Additional Info

--- Remake_Print.py
+++ Remake_Print.py
@@ -3,3 +3,6 @@ your_dream_job = str(input("Enter Your Dream Job: "))
 
 print("Hi" + your_name + "!")
 print("I hope you will become a " + your_dream_job + "!")
+
+your_birth_month = str(input(your_name + "!" + "When is your birth month? "))
+your_school = str(input("What school do you will pursue " + your_dream_job + "?: "))
