84671e810c6375eb09ecaf51c9213388f37369c4
Rei Rigos <mcrei1018@gmail.com>
2024-03-23 20:36:01 +0800

    Displaying The Additional Info With Style

--- Remake_Print.py
+++ Remake_Print.py
@@ -9,9 +9,11 @@ text_art_2 = (font_1.renderText("I hope you will become a " + your_dream_job + "
 
 print('\033[95m' + text_art_1 + text_art_2)
 
-your_birth_month = str(input(your_name + "!" + "When is your birth month?: "))
+font_2 = Figlet(font="cybermedium")
+your_birth_month = str(input(your_name + "! " + "When is your birth month?: "))
 your_school = str(input("What school do you will pursue " + your_dream_job + "?: "))
 
-print("Have a blast on " + your_birth_month + "!")
-print("Study Hard at " + your_school + "!")
+text_art_3 = (font_2.renderText("Have a blast on " + your_birth_month + "!"))
+text_art_4 = (font_2.renderText("Study Hard at " + your_school + "!"))
 
+print('\033[92m' + text_art_3 + text_art_4)
